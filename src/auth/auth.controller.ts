ad9325b4d492759a6026efd95844f4954506ddb9
Hung Pham <hungphammanh2012003@gmail.com>
2025-02-27 18:26:09 +0700

    video step 8

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './passport/local-auth.guard';
-import { JwtAuthGuard } from './passport/jwt-auth.guard';
+import { Public } from '@/decorator/customize';
+import { CreateAuthDto } from './dto/create-auth.dto';
 
 
 @Controller('auth')
@@ -9,14 +10,16 @@ export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
   @Post("login")
+  @Public()
   @UseGuards(LocalAuthGuard)
   async handleLogin(@Request() req) {
     return this.authService.login(req.user);
   }
 
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
+  // @UseGuards(JwtAuthGuard)
+  @Post('register')
+  @Public()
+  register(@Body() registerDto: CreateAuthDto) {
+    return this.authService.handleRegister(registerDto);
   }
 }
